d752dc940e02de9ede58c3b571efa88ac04e7959
nmk098 <nkuryumov@gmail.com>
2022-10-21 02:11:12 +0400

    1. adding view-connect with viewController 2. work with button configuration

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    
+    
+    @IBOutlet var changeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.layer.cornerRadius = 90
+        yellowView.layer.cornerRadius = 90
+        greenView.layer.cornerRadius = 90
+        
+        redView.alpha = 0.50
+        yellowView.alpha = 0.50
+        greenView.alpha = 0.50
+        
+        changeButton.configuration = setupButton(with: "START")
+    
     }
 
-
+    @IBAction func changeButtonTapped() {
+        changeButton.configuration = setupButton(with: "NEXT")
+    }
+    
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
+        buttonConfiguration.title = title
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        
+        return buttonConfiguration
+    }
+    
 }
 
