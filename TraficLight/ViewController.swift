d888d3b645136b50f779a989bea5df2ad7a22798
nmk098 <nkuryumov@gmail.com>
2022-10-21 16:04:42 +0400

    add count var

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -8,14 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
-    
-    
-    
     @IBOutlet var changeButton: UIButton!
+    
+    var tappCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,29 +23,53 @@ class ViewController: UIViewController {
         yellowView.layer.cornerRadius = 90
         greenView.layer.cornerRadius = 90
         
-        redView.alpha = 0.50
-        yellowView.alpha = 0.50
-        greenView.alpha = 0.50
+        redView.alpha = 0.30
+        yellowView.alpha = 0.30
+        greenView.alpha = 0.30
+        
+        tappCount = 0
         
         changeButton.configuration = setupButton(with: "START")
-    
+        changeButton.layer.cornerRadius = 10
     }
-
+    
     @IBAction func changeButtonTapped() {
         changeButton.configuration = setupButton(with: "NEXT")
+        tappCount += 1
+        changeAlpha()
     }
     
+   private func changeAlpha() {
+        if tappCount == 1 {
+            redView.alpha = 1
+            greenView.alpha = 0.30
+        } else if tappCount == 2 {
+            redView.alpha = 0.30
+            yellowView.alpha = 1
+            greenView.alpha = 0.30
+        } else if tappCount == 3 {
+            redView.alpha = 0.30
+            yellowView.alpha = 0.30
+            greenView.alpha = 1
+        } else if tappCount > 3 {
+            tappCount = 1
+            redView.alpha = 1
+            greenView.alpha = 0.30
+        }
+    }
     private func setupButton(with title: String) -> UIButton.Configuration {
         var buttonConfiguration = UIButton.Configuration.filled()
         
         buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.3333333433, green: 0.3333333433, blue: 0.3333333433, alpha: 1)
         buttonConfiguration.title = title
-        buttonConfiguration.buttonSize = .large
         buttonConfiguration.cornerStyle = .large
         buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
         
         return buttonConfiguration
     }
     
+    
+    
+    
+    
 }
-
